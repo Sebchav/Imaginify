3dd024658e210934982d030851d84c94beed1486
Sebchav <schavarria13@alumnos.uaq.mx>
2024-03-11 08:49:04 -0600

    Layouts modified

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 import React from 'react'
-import Layout from '../../Layout'
 
 const SignInPage = () => {
   return (
-    <Layout>
         <SignIn />
-    </Layout>
   )
 }
 
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 import React from 'react'
-import Layout from '../../Layout'
 
 const SignUpPage = () => {
   return (
-    <Layout>
         <SignUp />
-    </Layout>
   )
 }
 
