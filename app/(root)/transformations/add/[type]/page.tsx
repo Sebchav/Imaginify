90264f70f309242c49f28f276764fcddaeacc6bf
Sebchav <schavarria13@alumnos.uaq.mx>
2024-03-12 15:22:29 -0600

    TransformationForm part 1

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,10 +2,17 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
 import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = ({params: {type}}:SearchParamProps) => {
-  
+const AddTransformationTypePage = async({params: {type}}:SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
+  console.log(userId)
+  if(!userId) redirect("/sign-in")
+
+  const user = await getUserById(userId);
   
   return (
     <>
@@ -14,7 +21,12 @@ const AddTransformationTypePage = ({params: {type}}:SearchParamProps) => {
       subtitle={transformation.subTitle}
       />
 
-      <TransformationForm />
+      <TransformationForm 
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
     </>
   )
 }
