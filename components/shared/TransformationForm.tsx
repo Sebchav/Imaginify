4be02377834a78b124dc48390c19af06bb9a9ae2
Sebchav <schavarria13@alumnos.uaq.mx>
2024-03-20 15:25:51 -0600

    Changes

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -33,6 +33,7 @@ import TransformedImage from "./TransformedImage"
 import { getCldImageUrl } from "next-cloudinary"
 import { addImage, updateImage } from "@/lib/actions/image.actions"
 import { useRouter } from "next/navigation"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
 
 export const formSchema = z.object({
   title: z.string(),
@@ -179,13 +180,20 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     setnewTransformation(null);
 
     startTransition(async()=> {
-      await updateCredits(userId, -1)
+      await updateCredits(userId, creditFee)
     })
   }
+
+  useEffect(()=> {
+    if(image && (type === "restore" || type === "removeBackground")){
+      setnewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type])
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        { creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField 
           control={form.control}
           name="title"
